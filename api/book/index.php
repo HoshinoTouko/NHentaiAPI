0fc12da535d07245271c691ef4cd51b619451723
Touko <oranly@hotmail.com>
2015-10-11 14:03:25 +0900

    tags bug fixed

--- api/book/index.php
+++ api/book/index.php
@@ -2,19 +2,19 @@
 //头部保留信息
 //HoshinoTouko
 //2015-10-02 18:20
-include_once("../config.php");
-$bookID = $_GET['id'];
-$MACMD5 = $_GET['mac'];
-if ($bookID == '')
-{
-	$dieObj = array("error" => "NoID");
-	echo json_encode($dieObj);
-	die;
-}
+	include_once("../config.php");
+	$bookID = $_GET['id'];
+	$MACMD5 = $_GET['mac'];
+	if ($bookID == '')
+	{
+		$dieObj = array("error" => "NoID");
+		echo json_encode($dieObj);
+		die;
+	}
 
 
-header('content-type:text/html;charset="utf-8"');
-error_reporting(0);
+	header('content-type:text/html;charset="utf-8"');
+	error_reporting(0);
 ?>
 
 <?php
@@ -26,18 +26,16 @@ error_reporting(0);
 
 <?php 
 //引用simple_html_dom
-include_once("../simple_html_dom.php"); 
+	include_once("../simple_html_dom.php"); 
 
 //返回信息
-echo urldecode(getGallery($bookID));
+	echo urldecode(getGallery($bookID));
 
 
 ?>
 
 <?php
 //Functions
-
-
 function getGallery($bookID)
 {
 	//Load url
@@ -61,7 +59,6 @@ function getGallery($bookID)
 	$tempTextForTitle = $html->find('div[id=info]',0);
 	$title = GetBetween($tempTextForTitle, '<h1>', '</h1>');
 	$titleJP = GetBetween($tempTextForTitle, '<h2>', '</h2>');
-
 	
 	//uploadTime
 	$uploadTime = GetBetween($html, 'datetime="', '">');
@@ -70,33 +67,19 @@ function getGallery($bookID)
 	$uploadTime = "";
 	
 	//Parodies Characters Tags Artists Groups Language Category
-
-$tagshtml = $html->find('div[id=info]', 0);
-//debug
-//echo $tagshtml;
-//echo $html;
-//debug end
-$tagscount = substr_count($tagshtml, 'class="field-name"')
-;
-
-echo $tagscount;die;
-while ($tagscount >= 0)
-{
-getClassNamedFieldName($tagshtml, $tagscount - 1);
-$tagscount --;
-}
-
-
-//  $Parodies = getClassNamedFieldName($html, 0);
-//	$Characters = getClassNamedFieldName($html, 1);
-//	$Tags = getClassNamedFieldName($html, 2);
-//	$Artists = getClassNamedFieldName($html, 3);
-//	$Groups = getClassNamedFieldName($html, 4);
-//	$Language = getClassNamedFieldName($html, 5);
-//	$Category = getClassNamedFieldName($html, 6);
-
-	
-	
+	$tagscount = substr_count($html, 'class="field-name"');
+	//debug
+	//echo $tagscount . '<br>' ;
+	//if ( $tagscount != 7){echo 'Please Wait.';die;}
+	//debug end
+	while($tagscount >= 0)
+	{
+		$tempText = $html->find('div[class=field-name]', $tagscount-1);
+		$tagName = GetBetween($tempText , 'class="field-name">', ':' );
+		//echo $tagName;
+		$$tagName = getClassNamedFieldName($tempText, $tagscount-1);
+		$tagscount--;
+	}
 	
 	//uploadTime
 	$uploadTime = GetBetween($html, 'datetime="', '">');
@@ -111,7 +94,7 @@ $tagscount --;
 		//Parodies Characters Tags Artists Groups Language Category
 		"Parodies" => $Parodies,
 		"Characters" => $Characters,
-		"tags" => $tags,
+		"Tags" => $Tags,
 		"Artists" => $Artists,
 		"Group" => $Groups,
 		"Language" => $Language,
@@ -144,23 +127,19 @@ $tagscount --;
 	
 }
 
-function getClassNamedFieldName($temphtml, $num)
+function getClassNamedFieldName($tempText, $num)
 {
-	$tempText = $temphtml->find('div[class=field-name]', $num);
-
-  $fieldNameResult = GetBetween($tempText , 'class="field-name">', ':' );
-echo  $fieldNameResult ;die;
-   global $$fieldNameResult = array();
 	
+	$result = array();
 	$jj = 0;
 	do
 	{
 		$tempItem = trim(GetBetween($tempText->find('a', $jj), '>', ' <span')); 
-		array_push($$fieldNameResult, $tempItem);
+		array_push($result, $tempItem);
 		$jj++;
 	}
 	while(GetBetween($tempText->find('a', $jj), '>', ' <span') != '');
-	//return $fieldNameResult;
+	return $result;
 }
 
 
